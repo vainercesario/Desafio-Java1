77dee608fa85150427cf18ca47f6e04376f83232
vainercesario <c.vainer@gmail.com>
2023-08-08 08:37:56 -0300

    first commit

---
+++ src/main/java/com/vainercesario/desafio1/desafio1/Desafio1Application.java
@@ -0,0 +1,19 @@
+package com.vainercesario.desafio1.desafio1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Ola mundo");
+	}
+
+}
